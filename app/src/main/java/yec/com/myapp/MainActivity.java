c1e108e07368c5c0afe0d87760e9332ab7c23ecb
YeCHenliu <564244875@qq.com>
2016-12-31 17:41:22 +0800

    测试2

--- app/src/main/java/yec/com/myapp/MainActivity.java
+++ app/src/main/java/yec/com/myapp/MainActivity.java
@@ -2,12 +2,14 @@ package yec.com.myapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.e("MainActivity", "onCreate: ");
         setContentView(R.layout.activity_main);
     }
 }
